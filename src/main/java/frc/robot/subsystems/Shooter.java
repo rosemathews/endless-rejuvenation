863c5bd30b281442ec65a4e4f25a5660f1149c51
Robert Paul <rpaul@cusd.me>
2020-01-09 18:35:39 -0800

    Shooter Subsystem Created

---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,37 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Shooter extends SubsystemBase {
+  
+  private SpeedController m_Shooter;
+
+  /**
+   * Creates a new Shooter.
+   */
+  public Shooter() {
+    m_Shooter = new Talon(4);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+  
+  public void on() {
+    m_Shooter.set(1);
+  }
+
+  public void stop() {
+    m_Shooter.set(0);
+  }
+}
