190e02628313f7daa4cf5b0a367d9cc49ff088a1
Robert Paul <rpaul@cusd.me>
2020-02-08 14:01:51 -0800

    Changes to add fire command

---
+++ src/main/java/frc/robot/commands/turret/Fire.java
@@ -0,0 +1,45 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.turret;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Turret;
+
+public class Fire extends CommandBase {
+  /**
+   * Creates a new fire.
+   */
+  private Turret t;
+  public Fire(Turret t) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(t);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    t.shoot();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    t.stop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
